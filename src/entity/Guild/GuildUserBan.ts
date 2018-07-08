67b1ce950a00d27c9c2d1cec45b3cf39ec960ca7
jasonhaxstuff <shadowmanguyyt@gmail.com>
2018-07-08 16:39:29 -0400

    Create GuildUserBan table.

---
+++ src/entity/Guild/GuildUserBan.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  JoinColumn,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+import { GuildUser } from './GuildUser'
+
+@Entity()
+export class GuildUserBan {
+  @PrimaryGeneratedColumn() id: number
+
+  @Column('varchar')
+  issuerId: string
+
+  @Column('timestamp without time zone')
+  timestamp: Date
+
+  @Column('varchar')
+  reason: string
+
+  @Column('boolean')
+  unbanned: boolean
+
+  @Index({ unique: true })
+  @ManyToOne(type => GuildUser, guildUser => guildUser.bans)
+  @JoinColumn()
+  user: GuildUser
+
+  constructor(guildUserBan?: GuildUserBan) {
+    if (guildUserBan) {
+      Object.assign(this, guildUserBan)
+    }
+    this.timestamp = new Date()
+  }
+}

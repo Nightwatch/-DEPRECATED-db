6bd4966994af5ea29a37609f3f5a804e5ff03e21
jasonhaxstuff <shadowmanguyyt@gmail.com>
2018-07-08 16:40:57 -0400

    Create GuildUser table.

---
+++ src/entity/Guild/GuildUser.ts
@@ -0,0 +1,53 @@
+import {
+  Entity,
+  Column,
+  OneToOne,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+import { Guild } from '.'
+import { GuildUserWarning } from './GuildUserWarning'
+import { GuildUserKick } from './GuildUserKick'
+import { GuildUserBan } from './GuildUserBan'
+import { GuildUserReputation } from './GuildUserReputation'
+import { User } from '../User'
+
+@Entity()
+export class GuildUser {
+  @PrimaryGeneratedColumn() id: number
+
+  @Column('varchar', { length: 100 })
+  nickname: string
+
+  @Column('timestamp without time zone')
+  dateJoined: Date
+
+  @Column('timestamp without time zone', { nullable: true })
+  dateLastMessage: Date | null
+
+  @OneToMany(type => GuildUserWarning, guildUserWarning => guildUserWarning.user)
+  warnings: GuildUserWarning[]
+
+  @OneToMany(type => GuildUserKick, guildUserKick => guildUserKick.user)
+  kicks: GuildUserKick[]
+
+  @OneToMany(type => GuildUserBan, guildUserBan => guildUserBan.user)
+  bans: GuildUserBan[]
+
+  @OneToOne(type => GuildUserReputation, guildUserReputation => guildUserReputation.user)
+  reputation: GuildUserReputation
+
+  @OneToOne(type => User)
+  user: User
+
+  @ManyToOne(type => Guild, guild => guild.users)
+  guild: Guild
+
+  constructor(guildUser?: GuildUser) {
+    if (guildUser) {
+      Object.assign(this, guildUser)
+    }
+    this.dateJoined = new Date()
+  }
+}

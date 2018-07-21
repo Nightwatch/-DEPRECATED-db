ffe285772cb3720e3024caef18a11248ca12968c
Jason Bothel <shadowmanguyyt@gmail.com>
2018-07-21 15:21:10 -0400

    Add Guild JSdocs.

--- src/entity/Guild/GuildUserBan.ts
+++ src/entity/Guild/GuildUserBan.ts
@@ -4,23 +4,59 @@ import { IsString, IsDate } from 'class-validator'
 
 @Entity()
 export class GuildUserBan {
+  /**
+   * The ID of the guild user ban. Auto-generated.
+   *
+   * @type {number}
+   * @memberof GuildUserBan
+   */
   @PrimaryGeneratedColumn() id: number
 
+  /**
+   * The ID of the user that issued the ban.
+   *
+   * @type {string}
+   * @memberof GuildUserBan
+   */
   @Column('varchar')
   @IsString()
   issuerId: string
 
+  /**
+   * The date the ban was issued.
+   *
+   * @type {Date}
+   * @memberof GuildUserBan
+   */
   @Column('timestamp without time zone')
   @IsDate()
   timestamp: Date
 
+  /**
+   * The reason the ban was issued.
+   *
+   * @type {string}
+   * @memberof GuildUserBan
+   */
   @Column('varchar')
   @IsString()
   reason: string
 
+  /**
+   * The length of the ban, e.g. `1h`, `1w`, etc.
+   *
+   * @type {string | null}
+   * @memberof GuildUserBan
+   */
   @Column('varchar', { nullable: true, length: 10 })
   length: string | null
 
+  /**
+   * The guild user that is/was banned.
+   *
+   * @type {GuildUser}
+   * @memberof GuildUserBan
+   */
   @Index({ unique: true })
   @ManyToOne(type => GuildUser, guildUser => guildUser.bans)
   @JoinColumn()
@@ -30,6 +66,7 @@ export class GuildUserBan {
     if (guildUserBan) {
       Object.assign(this, guildUserBan)
     }
+
     this.timestamp = new Date()
   }
 }
--- src/entity/Guild/GuildUserWarning.ts
+++ src/entity/Guild/GuildUserWarning.ts
@@ -4,20 +4,50 @@ import { IsString, IsDate } from 'class-validator'
 
 @Entity()
 export class GuildUserWarning {
+  /**
+   * The ID of the guild user warning. Auto-generated.
+   *
+   * @type {number}
+   * @memberof GuildUserWarning
+   */
   @PrimaryGeneratedColumn() id: number
 
+  /**
+   * The ID of the user that issued the warning.
+   *
+   * @type {string}
+   * @memberof GuildUserWarning
+   */
   @Column('varchar')
   @IsString()
   issuerId: string
 
+  /**
+   * The date that the warning was issued.
+   *
+   * @type {Date}
+   * @memberof GuildUserWarning
+   */
   @Column('timestamp without time zone')
   @IsDate()
   timestamp: Date
 
+  /**
+   * The reason that the warning was issued.
+   *
+   * @type {string}
+   * @memberof GuildUserWarning
+   */
   @Column('varchar')
   @IsString()
   reason: string
 
+  /**
+   * The guild user that was warned.
+   *
+   * @type {GuildUser}
+   * @memberof GuildUserWarning
+   */
   @Index({ unique: true })
   @ManyToOne(type => GuildUser, guildUser => guildUser.warnings)
   @JoinColumn()
